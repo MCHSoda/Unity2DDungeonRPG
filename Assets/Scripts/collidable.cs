65c272f81cac671eb14b16a76ea09f06733dd074
MCHSoda <mchgecko@gmail.com>
2021-07-23 21:54:41 -0400

    E

---
+++ Assets/Scripts/collidable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coll : MonoBehaviour
+{
+    public ContactFilter2D filter;
+    private BoxCollider2D boxCollider;
+    private Collider2D[] hits = new Collider2D[10];
+
+    protected void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+}
